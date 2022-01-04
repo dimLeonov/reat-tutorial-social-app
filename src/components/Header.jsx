59146faa18d830919ed600194615c5ac4bfcda47
dmitry.leonov <dim.leonov97@gmail.com>
2022-01-04 20:57:35 +0200

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+            <header className='header'>
+                <img src='https://api.freelogodesign.org/files/a2515550332942b1aef2af50b5698018/thumb/logo_200x200.png?v=0' alt='Logo Here'/>
+            </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,32 @@
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src='https://cdn.pixabay.com/photo/2021/12/11/07/59/hotel-6862159_960_720.jpg' alt='Profile Image'/>
+            </div>
+            <div>
+                <p>Avatar + Decription</p>
+            </div>
+            <div>
+                <p>My posts</p>
+                <div>
+                    <p>New Post</p>
+                </div>
+                <div>
+                    <div>
+                        <p>post 1</p>
+                    </div>
+                    <div>
+                        <p>post 2</p>
+                    </div>
+                    <div>
+                        <p>post 3</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    )
+}
+
+export default Profile;
